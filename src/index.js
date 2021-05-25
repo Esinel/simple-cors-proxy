77c23b479c255ae3f503f9da6fda8e3271313d27
CootieMonster <stefanos@snowball.digital>
2021-05-26 00:31:36 +0200

    added cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 express = require("express");
 const request = require('request');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
