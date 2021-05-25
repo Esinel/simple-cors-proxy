ee26e9c55cbde88a9baedb7eede636743b73db1c
Stefanos Lalic <stefanosengl@gmail.com>
2021-05-25 23:12:36 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+express = require("express");
+const request = require('request');
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get("/*", (req, res) => {
+  let pathToProxy = req.originalUrl;
+  pathToProxy = pathToProxy.substring(1);
+  request(
+    { url: pathToProxy},
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+app.listen(8080);
